f53476949d8fb8e2ee2dc3390041a2da1a2e6e21
Janaina Rapanelli <janaina_2012silva@hotmail.com>
2024-08-21 20:19:41 -0300

    estruturando layout

---
+++ src/js/sections.js
@@ -0,0 +1,21 @@
+
+// Função para carregar o conteúdo de um arquivo HTML e inseri-lo em um elemento
+function loadHTML(file, selector) {
+    fetch(file)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Falha ao carregar ' + file);
+        }
+        return response.text();
+      })
+      .then(data => {
+        document.querySelector(selector).innerHTML = data;
+      })
+      .catch(error => console.error('Erro ao carregar o HTML:', error));
+  }
+  
+  // Carregar o cabeçalho e o rodapé ao carregar o documento
+  document.addEventListener('DOMContentLoaded', () => {
+    loadHTML('src/html/pages/home/sections/01_hero.html', '#hero');
+
+  });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,70 +1,73 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content:  ["*.{html,js}",
-'./index.html'],
+  content: [
+    './**/**/*.html',
+    './**/*.js',
+  ],
   theme: {
-  colors: {
-      'primary': '#C10044',
-      'secondary': '#9F0AC2',
-      'tertiary': '#20CCF7',
-      'fundo': '#151925',
-      'fundo-dark': '#0E0E1A',
-      'black': '#000000',
-      'white': {
-        light: '#c9c9c9',
-        DEFAULT: '#ffffff',
-        dark: '#9b9b9b',
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#C10044',
+          light: '#D63A5E',
+          dark: '#90002D',
+        },
+        secondary: {
+          DEFAULT: '#9F0AC2',
+          light: '#C86BCC',
+          dark: '#6A058B',
+        },
+        tertiary: {
+          DEFAULT: '#20CCF7',
+          light: '#6EE0FF',
+          dark: '#1A9ABF',
+        },
+        fundo: {
+          DEFAULT: '#151925',
+          dark: '#0E0E1A',
+        },
+        black: {
+          light: '#c9c9c9',
+          DEFAULT: '#000000',
+          dark: '#9b9b9b',
+        },
+        white: {
+          light: '#c9c9c9',
+          DEFAULT: '#ffffff',
+          dark: '#9b9b9b',
+        },
+        orange: {
+          DEFAULT: '#E52F00',
+          dark: '#E82300',
+        },
+        blue: '#0077E5',
+        yellow: '#E5CE00',
+        green: '#00E57F',
       },
-      'orange': {
-        DEFAULT: '#E52F00',
-        dark: '#E82300'
-      },
-      'blue': '#0077E5',
-      'yellow': '#E5CE00',
-      'green': '#00E57F',
     },
-    container: {
-      center: true,
-      screens: {
-        sm: '600px',
-        md: '728px',
-        lg: '984px',
-        xl: '1040px',
-      },
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
-      },
-    },
-
-
-  
   },
   plugins: [
-  
     function ({ addComponents }) {
-        addComponents({
-            '.container': {
-                maxWidth: '100%',
-                '@screen sm': {
-                    maxWidth: '640px',
-                },
-                '@screen md': {
-                    maxWidth: '768px',
-                },
-                '@screen lg': {
-                    maxWidth: '1024px',
-                },
-                '@screen xl': {
-                    maxWidth: '1180px',
-                },
-            },
-
-        })
-    }
-],
-}
+      addComponents({
+        '.container': {
+          with: '100%',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1024px',
+          },
+          '@screen xl': {
+            maxWidth: '1180px',
+          },
+        },
+      });
+    },
+  ],
+};
